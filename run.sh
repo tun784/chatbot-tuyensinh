35e72d444012b0de6aaf9ecde80ab7f530c5df77
tun784 <thudientu782004@gmail.com>
2025-05-28 06:49:12 +0700

    06:49

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
 export FLASK_APP=app.py
 export FLASK_ENV=development
-export GOOGLE_API_KEY=GOOGLE_API_KEY
+# export GOOGLE_API_KEY=GOOGLE_API_KEY
 flask run --host=0.0.0.0 --port=5000
